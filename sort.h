e8e5675194bc1cf1b3fbe5f161a2ab8a7874b6a2
BeygonK <kipkoechbeygon@gmail.com>
2024-02-19 22:33:41 -0800

    Bubble sort algorithm

--- sort.h
+++ sort.h
@@ -1,3 +1,5 @@
+#ifndef SORT_H
+#define SORT_H
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -20,3 +22,4 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+#endif
